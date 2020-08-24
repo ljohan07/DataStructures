f0833bcd38e3be7e25367c4aae299c5b02825925
Livia Johan <ljohan@student06.cse.nd.edu>
2020-08-23 21:32:07 -0400

    CC 1 Update

--- CC1/Creative.cpp
+++ CC1/Creative.cpp
@@ -1,23 +1,29 @@
+// Author: Livia Johan
+// File: Creative.cpp
+
 #include "CreativeFunctions.cpp"
 
-int main(){
+void main(){
 	
 	int numRows;
 	int** pascal;
 	COUT << "How many levels do you want to calculate: ";
 	CIN>> numRows;
 
-
+	// create a Pascal's Triangle
 	pascal = input(numRows);
+	// prints the triangle
 	print_triangle(pascal, numRows);
 
 	COUT << ENDL;
 
 	// Creative portion
+	
+	// Calculates nCk
 	getCombos(pascal, numRows);
+	// Calculates possible number of combinations given a user-provided number of options
 	totalCombos(pascal, numRows);
 	
 	
 	delete pascal;
-	return 0;
 }
