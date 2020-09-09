1a8e16340cb9b5bb6b48945cef3957ad505c3f81
Livia Johan <ljohan@student00.cse.nd.edu>
2020-09-08 22:43:53 -0400

    Lab 4 Update

---
+++ Lab4/src/Lab4.cpp
@@ -0,0 +1,136 @@
+// Author: Livia Johan
+// File: Lab4.cpp
+
+#include "../include/MergeInt.h"
+/*
+void test1(){
+	COUT << "-------------------------" << ENDL;
+	COUT << "Test 1: " << ENDL;
+
+	VECTOR< VECTOR<int> > v = {{1,4}, {2,5}, {6,9}};
+
+	COUT << "Inputs: " << ENDL;
+	COUT << "Intervals: ";
+	printVecVec( v );
+
+	COUT << ENDL << "Solution: " << ENDL;
+	printVecVec( merge(v) );
+}
+void test2(){
+
+	COUT << "-------------------------" << ENDL;
+	COUT << "Test 2: " << ENDL;
+
+	VECTOR< VECTOR<int> > v = {{1,3}, {6,9}, {2,8}};
+
+	COUT << "Inputs: " << ENDL;
+	COUT << "Intervals: ";
+	printVecVec( v );
+
+	COUT << ENDL << "Solution: " << ENDL;
+	printVecVec( merge(v) );
+
+}
+void test3(){
+
+	COUT << "-------------------------" << ENDL;
+	COUT << "Test 3: " << ENDL;
+
+	VECTOR< VECTOR<int> > v = { {1,2}, {3,5}, {6,7}, {8,10}, {12,16}, {4,8} };
+
+	COUT << "Inputs: " << ENDL;
+	COUT << "Intervals: ";
+	printVecVec( v );
+
+	COUT << ENDL << "Solution: " << ENDL;
+	printVecVec( merge(v) );
+
+}
+
+void test4(){
+
+	COUT << "-------------------------" << ENDL;
+	COUT << "Test 4: " << ENDL;
+
+	VECTOR< VECTOR<int> > v = { {7,12}, {3,5}, {1,2}, {5,6}, {8,14}, {9,22}};
+
+	COUT << "Inputs: " << ENDL;
+	COUT << "Intervals: ";
+	printVecVec( v );
+
+	COUT << ENDL << "Solution: " << ENDL;
+	printVecVec( merge(v) );
+
+}
+*/
+
+void ex1(){
+
+	COUT << "-------------------------" << ENDL;
+	COUT << "Example 1: " << ENDL;
+
+	VECTOR< VECTOR<int> > v = { {1,3}, {2,6}, {8,10}, {15,18}};
+
+	COUT << "Inputs: " << ENDL;
+	COUT << "Intervals: ";
+	printVecVec( v );
+
+	COUT << ENDL << "Output: " << ENDL;
+	printVecVec( merge(v) );
+
+}
+void ex2(){
+
+	COUT << "-------------------------" << ENDL;
+	COUT << "Example 3: " << ENDL;
+
+	VECTOR< VECTOR<int> > v = { {1,4}, {4,5} };
+
+	COUT << "Inputs: " << ENDL;
+	COUT << "Intervals: ";
+	printVecVec( v );
+
+	COUT << ENDL << "Output: " << ENDL;
+	printVecVec( merge(v) );
+
+}
+void ex3(){
+
+	COUT << "-------------------------" << ENDL;
+	COUT << "Example 3: " << ENDL;
+
+	VECTOR< VECTOR<int> > v = { {44,46}, {31,35}, {2,9}, {5,7}, {10, 12}, {11, 22}};
+
+	COUT << "Inputs: " << ENDL;
+	COUT << "Intervals: ";
+	printVecVec( v );
+
+	COUT << ENDL << "Output: " << ENDL;
+	printVecVec( merge(v) );
+
+}
+void ex4(){
+
+	COUT << "-------------------------" << ENDL;
+	COUT << "Example 4: " << ENDL;
+
+	VECTOR< VECTOR<int> > v = { {1,2}, {3,4}, {2,5}, {10,15}, {6, 12}, {16, 22}, {18, 32}, {25, 46}};
+
+	COUT << "Inputs: " << ENDL;
+	COUT << "Intervals: ";
+	printVecVec( v );
+
+	COUT << ENDL << "Output: " << ENDL;
+	printVecVec( merge(v) );
+
+}
+int main(){
+	ex1();
+	ex2();
+	ex3();
+	ex4();
+
+
+
+	return 0;
+}
