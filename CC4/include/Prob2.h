d1139652bca102bb4c19f96e9f46233679c6b5b9
Livia Johan <ljohan@student06.cse.nd.edu>
2020-09-27 21:45:09 -0400

    CC 4 Update

--- CC4/include/Prob2.h
+++ CC4/include/Prob2.h
@@ -11,23 +11,29 @@
 
 int water(int heights [], int bars) {
   int ans = 0;
-  // stores
+  // stores indices of heights
   stack <int> height_index;
+  // iterate through each "bar"
   for(int i = 0; i < bars; ++i) {
     while(!height_index.empty() && heights[i] > heights[height_index.top()]) {
+      // stores top of stack & pops it
       int peak = heights[height_index.top()];
       height_index.pop();
       if(height_index.empty()) {
         break;
       }
+      // calculate how wide valley
       int width = i - height_index.top() - 1;
       int minHeight;
+      // min of current index of heights or the height at index stored @
+      // top of stack
       if(heights[i] < heights[height_index.top()]){
         minHeight = heights[i];
       }
       else {
         minHeight = heights[height_index.top()];
       }
+      // gets depth/height of the water that will be stored
       int valleyDepth = minHeight-peak;
       ans += valleyDepth * width;
     }
